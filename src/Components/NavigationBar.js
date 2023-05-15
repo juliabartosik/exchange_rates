45cb621505a519bef5f0ce0c6c894c6bced958b1
juliabartosik <78592915+juliabartosik@users.noreply.github.com>
2023-05-15 14:30:38 +0200

    Test

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -5,7 +5,7 @@ function NavigationBar () {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky='top'>
             <Container>
-            <Navbar.Brand className='mx-auto pl-2'>Front-end Currency Exchange 2023</Navbar.Brand>
+            <Navbar.Brand className='me-auto pl-2'>Front-end Currency Exchange 2023</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mx-auto">
